e2c0b2eafcceaed2f3832ad6b2ddfdaed37dee0c
Olivier Gendron <112414219+olivier987654@users.noreply.github.com>
2022-10-22 08:03:21 -0700

    2022_10_22
    2022_10_22

--- 2022-10-21/2022_10_21.py
+++ 2022-10-21/2022_10_21.py
@@ -1,41 +1,8 @@
-# Python Script to monitor network with UI and log to file
+# Drawdata experiment
 
-import os
-import time
-import datetime
-import subprocess
-import tkinter as tk
-from tkinter import ttk
+# Importing the drawdata 
+from drawdata import draw_scatter
 
-# Create a window
-window = tk.Tk()
-window.title("Network Monitor")
-window.geometry("500x500")
+# Drawing the scatter plot
 
-# Create a label
-label = tk.Label(text="Network Monitor")
-label.pack()
-
-# Create a button
-button = tk.Button(text="Start", width=25, command=window.destroy)
-button.pack()
-
-# Monitor network
-def monitor_network():
-    # Get current time
-    now = datetime.datetime.now()
-    # Create a file name with current time
-    file_name = now.strftime("%Y-%m-%d-%H-%M-%S") + ".txt"
-    # Create a file with current time
-    file = open(file_name, "w")
-    # Write the header to the file
-    file.write("Time,Status,IP Address,MAC Address")
-    
-    # Loop forever
-    while True:
-        # Get current time
-        now = datetime.datetime.now()
-        # Get current time in string format
-        current_time = now.strftime("%Y-%m-%d %H:%M:%S")
-        # Ping Google
-        response = os.system("ping -n 1")
+draw_scatter() # This library will allow you to show your team how data is drawn by the computer.
