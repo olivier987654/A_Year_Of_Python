b3cdef784e77cc7dcd8f1f16e2c4962428176be4
Olivier Gendron <gendron.olivier01@gmail.com>
2022-10-28 13:18:46 -0400

    Rock, paper, scissor game
    Rock paper & Scissor game

---
+++ 2022-10-28/2022-10-28.py
@@ -0,0 +1,40 @@
+## Python rock paper scissors game
+
+import random
+
+# Create a list of play options
+t = ["Rock", "Paper", "Scissors"]
+
+# Assign a random play to the computer
+
+computer = t[random.randint(0,2)]
+
+# Set player to False
+
+player = False
+
+while player == False:
+    # Set player to True
+    player = input("Rock, Paper, Scissors?")
+    if player == computer:
+        print("Tie!")
+    elif player == "Rock":
+        if computer == "Paper":
+            print("You lose :(", computer, "covers", player)
+        else:
+            print("You win!", player, "smashes", computer)
+    elif player == "Paper":
+        if computer == "Scissors":
+            print("You lose:(", computer, "cut", player)
+        else:
+            print("You win!", player, "covers", computer)
+    elif player == "Scissors":
+        if computer == "Rock":
+            print("You lose :(...", computer, "smashes", player)
+        else:
+            print("You win!", player, "cut", computer)
+    else:
+        print("Huh ? Not a valid play. Check your spelling!")
+    # Reset the game loop and start again
+    player = False
+    computer = t[random.randint(0,2)]
