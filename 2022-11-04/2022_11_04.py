67e5fae036efc8f8d5a8f1168f4a21f98688ec77
Olivier Gendron <gendron.olivier01@gmail.com>
2022-11-04 17:00:45 -0400

    2022_11_03
    2022 11 03

---
+++ 2022-11-04/2022_11_04.py
@@ -0,0 +1,22 @@
+## Calculate the number of steps in a algorithm to find the number of steps in a algorithm
+
+# Simple index for loop
+
+for i in range(10):
+    print(i)
+
+## The algorithm takes 10 steps to complete in that case
+
+## f(n) = 10
+
+## Keep track of the sum of all the numbers you are printing
+
+count = 0
+for i in range (10):
+    print(i)
+    count += i
+print(count)
+
+# This way we add the number found in the loop to the count variable
+
+## The algorithm takes 11 steps to complete in that case
---
+++ 2022-11-04/scratch_2.md
@@ -0,0 +1,4 @@
+Today is a project based on the book : the self-taught computer scientist
+
+We explore the concepts of algorithms and how to count the steps of an algorithm.
+
