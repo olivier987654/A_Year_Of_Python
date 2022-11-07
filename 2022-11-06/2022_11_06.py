7e5b0ae7254770d95714bfd075ec8067e2fa52c6
Olivier Gendron <gendron.olivier01@gmail.com>
2022-11-06 21:39:34 -0500

    2022_11_06
    2022_11_06

---
+++ 2022-11-06/2022_11_06.py
@@ -0,0 +1,11 @@
+## Python Script to reverse a string using a single line
+
+# Creating a string to reverse
+
+string = "This is the string we are going to reverse"
+
+# Print the reversed string
+
+print(string[::-1])
+
+# This print funtion will print the string in reverse using a single line.
