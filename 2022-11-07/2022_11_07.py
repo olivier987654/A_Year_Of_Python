bf841c78d4372fdf0381f44903007f840697f309
Olivier Gendron <gendron.olivier01@gmail.com>
2022-11-07 22:26:50 -0500

    2022_11_07
    2022-11-07

---
+++ 2022-11-07/2022_11_07.py
@@ -0,0 +1,56 @@
+# Python file with different data types
+
+# Creating a list
+
+list = [1, 2, 3, 4, 5]
+
+# Creating a tuple
+
+tuple = (1, 2, 3, 4, 5)
+ # The difference between a list and a tuple is that a list is mutable and a tuple is immutable.
+
+# Creating a dictionary
+
+dictionary = {"name": "John", "age": 30, "city": "New York"}
+# A dictionary is a collection which is unordered, changeable and indexed. In Python dictionaries are written with curly brackets, and they have keys and values.
+
+# Creating a set
+
+set = {"apple", "banana", "cherry"}
+# A set is a collection which is unordered and unindexed. In Python sets are written with curly brackets.
+
+# Complex data types --------------------------------------------------------------------------------------------------
+
+# Creating a list of lists
+
+list_of_lists = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+# Creating a list of tuples
+
+list_of_tuples = [(1, 2, 3), (4, 5, 6), (7, 8, 9)]
+
+# Creating a list of dictionaries
+
+list_of_dictionaries = [{"name": "John", "age": 30, "city": "New York"}, {"name": "Jane", "age": 25, "city": "Los Angeles"}, {"name": "Bob", "age": 40, "city": "Chicago"}]
+
+# Creating a list of sets
+
+list_of_sets = [{"apple", "banana", "cherry"}, {"orange", "mango", "grapes"}, {"watermelon", "pineapple", "strawberry"}]
+
+# Creating a tuple of lists
+
+tuple_of_lists = ([1, 2, 3], [4, 5, 6], [7, 8, 9])
+
+# Creating a tuple of tuples
+
+tuple_of_tuples = ((1, 2, 3), (4, 5, 6), (7, 8, 9))
+
+# Creating a tuple of dictionaries
+
+tuple_of_dictionaries = ({"name": "John", "age": 30, "city": "New York"}, {"name": "Jane", "age": 25, "city": "Los Angeles"}, {"name": "Bob", "age": 40, "city": "Chicago"})
+
+# Creating a tuple of sets
+
+tuple_of_sets = ({"apple", "banana", "cherry"}, {"orange", "mango", "grapes"}, {"watermelon", "pineapple", "strawberry"})
+
+# You can go on and create more complex data types like a list of tuples of lists of dictionaries of sets and so on.
