db6b93875dac78265cea1d3994662cd42641f3c0
Olivier Gendron <112414219+olivier987654@users.noreply.github.com>
2022-11-20 22:04:04 -0500

    2022_11_19
    2022-11-19

---
+++ 2022-11-20/2022_11_19.py
@@ -0,0 +1,13 @@
+## Today is a first,  I am going to try and make this script using an app on my phone
+
+
+## How to use ' in a string ?
+
+## \
+
+# EX : 
+
+print("This is ann\'s)
+
+## Output : This is ann's
+
